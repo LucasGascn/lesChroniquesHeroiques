1d7eefc71df54e8176c6a2c098c782aaaacc2aaf
Lucas <lucas.gascoin@edu.esiee-it.fr>
2023-04-18 15:06:54 +0200

    form test

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,9 +1,48 @@
+
+
+const FormPj = [{
+    inputName:'name1',
+    value : 'value1',
+    inputId : 'id1',
+    },
+    {
+    inputName:'name2',
+    value : 'value2',
+    inputId : 'id2',
+    },
+{
+    inputName:'value3',
+    value : 'value3',
+    inputId : 'id3',
+    },
+    {
+    inputName:'value4',
+    value : 'value4',
+    inputId : 'id4',
+    },
+    {
+    inputName:'value5',
+    value : 'value5',
+    inputId : 'id5',
+    },];
+
+
+    
 function Home() {
+    const FPJ = FormPj.map((FPJ) => (
+        <div key={FPJ.inputId}>
+        <label htmlFor={FPJ.inputId}>{FPJ.inputName}:</label>
+        <input type="text" id={FPJ.inputId} name={FPJ.inputName} value={FPJ.value} />
+        </div>
+    ));
+      
     return (
-      <div>
-        <h1>HOME</h1>
-      </div>
+        <div>
+        <form>
+            {FPJ}
+        </form>
+        </div>
     );
-  }
-  
+    }
+ 
   export default Home;
