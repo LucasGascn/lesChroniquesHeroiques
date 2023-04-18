62980ceccbcca7d171eba88f198274718be01a25
Iagomorph <philemon.wild@gmail.com>
2023-04-18 16:58:38 +0200

    feat(map): switch from sphere to hexagon

--- client/src/components/Hexmap.jsx
+++ client/src/components/Hexmap.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useRef} from 'react'
+import { CylinderGeometry } from 'three'
+import { BoxGeometry } from 'three'
+import { Vector2 } from 'three'
 // import { MeshBasicMaterial } from 'three'
 import { FloatType, MeshStandardMaterial, PMREMGenerator, SphereGeometry, Scene, PerspectiveCamera, WebGLRenderer, ACESFilmicToneMapping, sRGBEncoding, Mesh, Color} from 'three'
 import { RGBELoader, OrbitControls } from 'three-stdlib'
+import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils'
 
 
 // FloatType, MeshStandardMaterial, PMREMGenerator
@@ -14,18 +18,21 @@ const Hexmap = () => {
     
 
     const loadEnvMapTexture = async(renderer,envmap) => {
-        let envTexture = require("../assets/envmap.hdr")
         // renders the texture of our environment map
         let pmrem = new PMREMGenerator(renderer);
-        let envmapTexture = await new RGBELoader().setDataType(FloatType).loadAsync(envTexture)
+        let envmapTexture = await new RGBELoader().setDataType(FloatType).loadAsync()
         envmap = pmrem.fromEquirectangular(envmapTexture).texture;
         return envmap
     }
+
+    
 
 
     useEffect(()=>{
         const scene = new Scene()
         scene.background = new Color("honeydew")
+
+        let hexagonGeometries = new BoxGeometry(0,0,0) // stocks all hex to avoid overloading cpu
     
         const camera = new PerspectiveCamera(45, 1000 / 1000, 0.1, 1000)
         // camera.position.set(-17,31,33);
@@ -35,7 +42,7 @@ const Hexmap = () => {
         renderer.setSize(1000,1000)
         renderer.toneMapping = ACESFilmicToneMapping;
         renderer.outputEncoding = sRGBEncoding;
-        renderer.physicallyCorrectLights = true;
+        // renderer.physicallyCorrectLights = true;
 
         console.log(renderer)
 
@@ -50,23 +57,43 @@ const Hexmap = () => {
         loadEnvMapTexture(renderer,envmap)
 
 
-        let sphereMesh = new Mesh(
-            new SphereGeometry(5,10,10),
+        // let sphereMesh = new Mesh(
+        //     new SphereGeometry(5,10,10),
+        //     new MeshStandardMaterial({
+        //         envMap: envmap,
+        //         roughness: 0,
+        //         metalness:1,
+        //     })
+        //     // new MeshBasicMaterial({ color: 0xff0000})
+        // )
+        // scene.add(sphereMesh)
+        makeHex(3, new Vector2(0,0))
+        let hexagonMesh = new Mesh(
+            hexagonGeometries,
             new MeshStandardMaterial({
                 envMap: envmap,
-                roughness: 0,
-                metalness:1,
+                flatShading:true
             })
-            // new MeshBasicMaterial({ color: 0xff0000})
-        )
-        scene.add(sphereMesh)
-
+            )
+        scene.add(hexagonMesh)
         
         renderer.setAnimationLoop(()=>{
             controls.update()
             //renders the scene at 60fps
             renderer.render(scene, camera)
         })
+        
+        function hexGeometry(height, position){
+            let geo = new CylinderGeometry(1,1,height, 6, 1, false)
+            geo.translate(position.x,height * 0.5, position.y)
+
+            return geo
+        }
+
+        function makeHex(height, position){
+            let geo = hexGeometry(height, position)
+            hexagonGeometries = mergeBufferGeometries([hexagonGeometries, geo])
+        }
     },[])
 
     return(
