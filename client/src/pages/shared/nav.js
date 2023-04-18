e44563e51a8230966fd5a5ca6919bcefaf2e1a8c
Lucas <lucas.gascoin@edu.esiee-it.fr>
2023-04-18 14:46:24 +0200

    add front branch

---
+++ client/src/pages/shared/nav.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function logo(){
+    return(
+        <Link to="/">
+            <img src="logo.png" alt="Logo" />
+        </Link>
+    )
+}
+function Nav(){
+    return(
+        <>
+            <nav>
+
+            </nav>
+        </>
+    )
+}
