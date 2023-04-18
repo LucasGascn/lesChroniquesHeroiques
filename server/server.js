2ebff4c4747bb03f30c0ef52d70fef5638a47d0a
Jules <jules.duarte@edu.esiee-it.fr>
2023-04-18 15:19:47 +0200

    login/signup/logout

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 app.use(express.json());
+app.use(cors());
 
 const mongoUrl =
-  "mongodb+srv://admin:OqCfT4snSKtMY45S@cluster0.nrlwnj8.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:OqCfT4snSKtMY45S@cluster0.nrlwnj8.mongodb.net/ProjetWeb";
+
+const JWT_SECRET = "efkffjzehfjhzeufhzufhziofhziofhuerhfuzofuhzf";
 
 mongoose
   .connect(mongoUrl, {
@@ -29,8 +35,29 @@ app.post("/register", async (req, res) => {
     });
     res.send({ status: "ok" });
   } catch (error) {
-    res.send({ status: "error" });
+    res.status(500).send({ status: error });
   }
+});
+
+app.post("/auth", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.send({ status: "error", error: "user not found" });
+  }
+
+  if (user.password === password) {
+    const token = jwt.sign({ user }, JWT_SECRET);
+
+    if (res.status(201)) {
+      return res.send({ token: token, status: "ok" });
+    } else {
+      return res.send({ error: "error" });
+    }
+  }
+  res.send({ status: "error", error: "Mauvais mot de passe" });
 });
 
 app.listen(5000, () => {
