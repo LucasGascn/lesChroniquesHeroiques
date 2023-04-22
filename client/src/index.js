03a01ca5753edfba24b492510fe552a4e123dc55
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-04-22 19:26:29 +0200

    fix background image size + nav image

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./bootstrapCustom.scss";
+import "./styles/bootstrapCustom.scss";
 import "./styles/global.css";
 import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
