62f3e695baea429b44be16fcaff3d4caa95dbdf9
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-04-24 10:10:58 +0200

    home animation fade-in + fix background

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,6 +1,6 @@
 import Save from "./save";
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import FadeIn from "../shared/fade-in";
 
 
 const listAventures = [{
@@ -41,13 +41,16 @@ const Home = ({ ...props }) => {
             </div>
         </div>
         <div className="home__desc">
-            <p id="home__desc__1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis ullamcorper velit. Quisque varius justo vel tempus tincidunt. Sed at ligula eu quam pulvinar semper. Etiam luctus, ipsum venenatis bibendum consectetur, leo dui hendrerit neque, sagittis volutpat risus neque et metus. Donec malesuada fermentum tellus non faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc non fermentum mauris. Cras eget convallis urna. Aliquam erat volutpat. Phasellus suscipit ornare nisi.
-            </p>
-            <p id="home__desc__2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis ullamcorper velit. Quisque varius justo vel tempus tincidunt. Sed at ligula eu quam pulvinar semper. Etiam luctus, ipsum venenatis bibendum consectetur, leo dui hendrerit neque, sagittis volutpat risus neque et metus. Donec malesuada fermentum tellus non faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc non fermentum mauris. Cras eget convallis urna. Aliquam erat volutpat. Phasellus suscipit ornare nisi.
-            </p>
-
+            <FadeIn direction='right'>
+                <p className="description description--right">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis ullamcorper velit. Quisque varius justo vel tempus tincidunt. Sed at ligula eu quam pulvinar semper. Etiam luctus, ipsum venenatis bibendum consectetur, leo dui hendrerit neque, sagittis volutpat risus neque et metus. Donec malesuada fermentum tellus non faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc non fermentum mauris. Cras eget convallis urna. Aliquam erat volutpat. Phasellus suscipit ornare nisi.
+                </p>
+            </FadeIn>
+            <FadeIn direction='left'>
+                <p className="description description--left">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis ullamcorper velit. Quisque varius justo vel tempus tincidunt. Sed at ligula eu quam pulvinar semper. Etiam luctus, ipsum venenatis bibendum consectetur, leo dui hendrerit neque, sagittis volutpat risus neque et metus. Donec malesuada fermentum tellus non faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc non fermentum mauris. Cras eget convallis urna. Aliquam erat volutpat. Phasellus suscipit ornare nisi.
+                </p>
+            </FadeIn>
         </div>
     </div>
         ;
