6bab5338274385ff90654d8fbb943562e72173a3
Lucas <lucas.gascoin@edu.esiee-it.fr>
2023-04-28 13:22:56 +0200

    push rest of front

--- client/src/pages/mj/addClasses.js
+++ client/src/pages/mj/addClasses.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
 function AddClass(props){
-    
+    const [completeClass, setCompleteClass] = useState({
+        name: '',
+        stat :[]
+    })
     const [className, setClassName] = useState('') 
     const [statName, setStatName] = useState('') 
     const [statBase, setStatBase] = useState('') 
@@ -10,49 +13,92 @@ function AddClass(props){
 
     const submitClass = async (event) => {
         event.preventDefault();
-        if (!className || !statName || !statBase || !statMax) {
-          setError('Veuillez remplir tous les champs');
+        if (!className) {
+          setError('Veuillez remplir le champ du noms de classe');
           return;
         }
         const newClass = {
             name: className,
+        };
+        try {
+            props.adventure.classes.push(newClass)
+          await axios.post(`/updateAdventure/${props.adventure._id}`, { adventure : newClass });
+          console.log('Une nouvelle classe a été ajouté');
+          setClassName('');
+          console.log(props.adventure._id);
+        } catch (error) {
+            console.error("Echec d'ajout de PNJ", error);
+        }
+      };
+      const submitStat = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget)
+        console.log(data.get('name'))
+        if(!statName || !statBase || !statMax){
+            setError('veuillez remplir tous les champs')
+            return;
+        }
+        const newStat = {
             stats :
             {
                 name: statName,
                 value: statBase,
                 max: statMax
             }
-            
-        };
-        try {
-            props.adventure.classes.push(newClass)
-          await axios.post(`/updateAdventure/${props.adventure._id}`, { classe: newClass });
-          console.log('Une nouvelle classe a été ajouté');
-          setClassName('');
-          setStatName('');
-          setStatBase('');
-          setStatMax('');
-          console.log(props.adventure._id);
-        } catch (error) {
-            console.error("Echec d'ajout de PNJ", error);
         }
-      };
+        console.log(completeClass)
+        const tempClass = completeClass;
+        tempClass.stat.push(newStat)
+        setCompleteClass(tempClass)
+      }
 
+      const submitClassName = (event) =>{
+        event.preventDefault();
+        if(!className){
+            setError('veuillez remplir tous les champs')
+            return;
+        }
+        const tempClass = completeClass;
+        tempClass.name = className
+        console.log(tempClass)
+        
+        setCompleteClass(tempClass)
+        console.log(completeClass)
+      }
 
     return(
     <>
       <h3>Définissez les classes</h3>
-      <form onSubmit={submitClass} style={{width:'21vw'}}>
-        Classe : <input type="text" name="nom" value={className} onChange={(e) => setClassName(e.target.value)} />
-        <p>définissez les stats des classes (Vie, Mana, force, etc...)</p>
-        Nom de la stats : <input type="text" name="statName" value={statName} onChange={(e) => setStatName(e.target.value)} />
+      <div className="d-flex">
+      <div className="w-100 p-1">
+        <form onSubmit={submitClassName}>
+            Classe : <input type="text" name="nom" value={className} id="name" onChange={(e) => setClassName(e.target.value)} />
+            <button type="submit">Ajouter la classe</button>
 
-        Valeur de base : <input type="text" name="statBase" value={statBase} onChange={(e) => setStatBase(e.target.value)} />
-        Valeur maximal : <input type="text" name="statMax" value={statMax} onChange={(e) => setStatMax(e.target.value)} />
-        <button type="submit">Ajouter la stats</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-
-    </form>
-
+        </form>
+        <form className="classes__form" onSubmit={submitStat}>
+            <p>Définissez les stats des classes (Vie, Mana, force, etc...) : </p>
+            Nom de la stats : <input type="text" name="statName" value={statName} onChange={(e) => setStatName(e.target.value)} />
+            Valeur de base : <input type="text" name="statBase" value={statBase} onChange={(e) => setStatBase(e.target.value)} />
+            Valeur maximal : <input type="text" name="statMax" value={statMax} onChange={(e) => setStatMax(e.target.value)} />
+            <button type="submit">Ajouter la stats</button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+        </form>
+      </div>
+      <div>
+        <span>
+            {completeClass.name}
+        </span>
+        <div>
+            {completeClass.stat.map(element =>{
+                <span>
+                    {element.name}
+                </span>
+            })}
+        </div>
+      </div>
+      </div>
+    
+    
     </>)
 }export default AddClass;
