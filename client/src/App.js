3f0a4fbfc17a42677db9c4c4bb5d7df877eba58f
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-04-28 13:17:07 +0200

    add custom mui+ details design home and lobby

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import RoutesProvider from "./routes";
 import NavBar from "./pages/shared/nav";
 import Background from "./background";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/muiCustom';
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
       <Background></Background>
       <NavBar></NavBar>
       <RoutesProvider></RoutesProvider>
+      </ThemeProvider>
     </div>
   );
 }
